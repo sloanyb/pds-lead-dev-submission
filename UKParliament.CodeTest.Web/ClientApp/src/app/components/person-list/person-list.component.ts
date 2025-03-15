c2769f179269729134fcd1c032e88bc7ff49e687
Ben Sloan <bgs264@gmail.com>
2025-03-15 18:59:42 +0000

    An initial commit of code to call the get-all people API endpoint- which doesn't exist so it's currently erroring. Next step is to add that missing API endpoint.

--- UKParliament.CodeTest.Web/ClientApp/src/app/components/person-list/person-list.component.ts
+++ UKParliament.CodeTest.Web/ClientApp/src/app/components/person-list/person-list.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PersonService } from '../../services/person.service';
+import { PersonViewModel } from '../../models/person-view-model';
 
 @Component({
   selector: 'app-person-list',
   standalone: false,
   templateUrl: './person-list.component.html',
-  styleUrl: './person-list.component.scss'
+  styleUrls: ['./person-list.component.scss']  // Note: plural 'styleUrls' is expected
 })
-export class PersonListComponent {
+export class PersonListComponent implements OnInit {
+  people: PersonViewModel[] = [];
 
+  constructor(private personService: PersonService) { }
+
+  ngOnInit(): void {
+    this.personService.getAll().subscribe(
+      (data: PersonViewModel[]) => {
+        this.people = data;
+      },
+      error => {
+        console.error('Error fetching people:', error);
+      }
+    );
+  }
 }
--- UKParliament.CodeTest.Web/ClientApp/src/app/services/person.service.ts
+++ UKParliament.CodeTest.Web/ClientApp/src/app/services/person.service.ts
@@ -9,8 +9,11 @@ import { PersonViewModel } from '../models/person-view-model';
 export class PersonService {
   constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) { }
 
-  // Below is some sample code to help get you started calling the API
   getById(id: number): Observable<PersonViewModel> {
     return this.http.get<PersonViewModel>(this.baseUrl + `api/person/${id}`)
+  }
+
+  getAll(): Observable<PersonViewModel[]> {
+    return this.http.get<PersonViewModel[]>(this.baseUrl + 'api/person');
   }
 }

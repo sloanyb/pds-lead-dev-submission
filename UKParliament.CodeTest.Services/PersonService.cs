7f0ebe9c7ac5344256b10a218a9d2a5ae30f8320
Ben Sloan <bgs264@gmail.com>
2025-03-15 09:18:04 +0000

    Test failing for the right reason

--- UKParliament.CodeTest.Services/PersonService.cs
+++ UKParliament.CodeTest.Services/PersonService.cs
@@ -1,8 +1,14 @@
-﻿using UKParliament.CodeTest.Data.Repository;
+﻿using UKParliament.CodeTest.Data.Model;
+using UKParliament.CodeTest.Data.Repository;
 
 namespace UKParliament.CodeTest.Services;
 
 public class PersonService(IPersonRepository personManagerRepository) : IPersonService
 {
     private readonly IPersonRepository _personManagerRepository = personManagerRepository;
+
+    public Person GetPersonById(int personId)
+    {
+        throw new NotImplementedException();
+    }
 }
